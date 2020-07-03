bcc112215b30c1c25e2aba3db4b99f795b262f62
DoThuyLinh <dothuylinh2105@gmail.com>
2020-07-04 00:30:46 +0700

    api

--- SOURCECODE/KTTKPM_DHKTPM13B_BAITAPLON_NHOM13/ATM.WebApi/BusinessLogic/BLL/UserLoginBLL.cs
+++ SOURCECODE/KTTKPM_DHKTPM13B_BAITAPLON_NHOM13/ATM.WebApi/BusinessLogic/BLL/UserLoginBLL.cs
@@ -26,49 +26,55 @@ namespace BusinessLogic.BLL
         }
         public ApiUserLoginModel UserLogin(string acc, string pass, int atmID)
         {
-            var person = new Person();
-            var accountCard = new AccountCard();
-            UserLogin userLogin = _userLogin.GetByCondition(x => x.AccountNumber.Equals(acc));
+            ApiUserLoginModel userloginmodel = new ApiUserLoginModel();
+            userloginmodel.ApiPersonModel = new ApiPersonModel(null, null);
+            
 
+            UserLogin userLogin = _userLogin.GetByCondition(x => x.AccountNumber.Equals(acc));
+            AccountCard accountCard = _accountCard.GetByCondition(x => x.AccountNumber.Equals(userLogin.AccountNumber));
             if (userLogin.CountPassword > 3)
             {
                 UpdateAccountByCountPass(accountCard);
-                return null;
+                userloginmodel.ErrorMessages = new List<string> (){ "false","tai khoan da khoa"};
+                return userloginmodel;
             }
             else if (userLogin.Password.Equals(pass))
             {
-                accountCard = _accountCard.GetByCondition(x => x.AccountNumber.Equals(userLogin.AccountNumber));
-                person = _person.GetByCondition(x => x.PersonID == accountCard.PersonID);
+                var person = _person.GetByCondition(x => x.PersonID == accountCard.PersonID);
+                userloginmodel.ApiPersonModel = _personBLL.PersonInfo(accountCard);
+                if (person is Staff)
+                {
+                    userloginmodel.Role = ApiUserLoginModel.AccountRole.Staff;
+                }
+                else if (person is Customer)
+                {
+                    userloginmodel.Role = ApiUserLoginModel.AccountRole.Customer;
+                    userloginmodel.ApiPersonModel = new ApiPersonModel(acc, person.PersonName);
+                }
+                try
+                {
+                    _addHistoryBLL.AddATMHistory(atmID);
+                    _addHistoryBLL.AddAccountHistory(accountCard.AccountNumber);
+                    return userloginmodel;
+                }
+                catch (Exception)
+                {
+                    return userloginmodel;
+                }
             }
             else
             {
-                userLogin.CountPassword += 1;
+                UpdateAccountStatusByCountPass(userLogin);
             }
-
-            ApiUserLoginModel userloginmodel = new ApiUserLoginModel();
-
-            userloginmodel.ApiPersonModel = _personBLL.PersonInfo(accountCard);
-            if (person is Staff)
-            {
-                userloginmodel.Role = ApiUserLoginModel.AccountRole.Staff;
-            }
-            else if (person is Customer)
-            {
-                userloginmodel.Role = ApiUserLoginModel.AccountRole.Customer;
-                userloginmodel.ApiPersonModel = new ApiPersonModel(acc, person.PersonName);
-            }
-            try
-            {
-                _addHistoryBLL.AddATMHistory(atmID);
-                _addHistoryBLL.AddAccountHistory(accountCard.AccountNumber);
-                return userloginmodel;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+            return userloginmodel;
+            
         }
-
+        public bool UpdateAccountStatusByCountPass(UserLogin userLogin)
+        {
+            userLogin.CountPassword += 1;
+            _userLogin.Update(userLogin);
+            return true;
+        }
         public bool UpdateAccountByCountPass(AccountCard account)
         {
             account.Status = AccountCard.AccountStatus.Pause;
